d7ac557ec4fcdf039083292d4750c45b7cf8176a
tandera <tandera@notemail.com>
2024-04-11 21:13:33 -0300

    feat: public profile service created

--- nextlinktree-front/src/app/[username]/page.tsx
+++ nextlinktree-front/src/app/[username]/page.tsx
@@ -3,10 +3,11 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 import Link from "next/link";
+import { getPublicProfile } from "./services/PublicProfileService";
 
 export default function PublicProfile({ params }: { params: { username: string } }) {
 
-    const username = params.username;
+    const profile = getPublicProfile(params.username);
 
     return (
         <div className="flex h-screen">
@@ -15,23 +16,21 @@ export default function PublicProfile({ params }: { params: { username: string }
                     <div className="pt-5">
                         <div className="flex justify-center">
                             <Avatar className="text-center">
-                                <AvatarImage className="h-20 w-20 rounded-full" src="https://github.com/shadcn.png" alt="@shadcn" />
-                                <AvatarFallback>CN</AvatarFallback>
+                                <AvatarImage className="h-20 w-20 rounded-full" src={profile.imageURL} alt="@shadcn" />
+                                <AvatarFallback>{profile.username}</AvatarFallback>
                             </Avatar>
                         </div>
                         
                         <CardHeader className="text-center">
-                            <CardTitle>{username}</CardTitle>
-                            <CardDescription>Marquei oftalmologista</CardDescription>
+                            <CardTitle>{profile.username}</CardTitle>
+                            <CardDescription>{profile.description}</CardDescription>
                         </CardHeader>
                     </div>
                     <div>
                         <CardContent className="flex flex-col gap-2">
-                            <Card className="w-[300px] text-center p-2"><Link target="_blank" href={"https://facebook.com"}>Facebook</Link></Card>
-                            <Card className="w-[300px] text-center p-2"><Link target="_blank" href={"https://instagram.com"}>Instagram</Link></Card>
-                            <Card className="w-[300px] text-center p-2"><Link target="_blank" href={"https://web.whatsapp.com"}>Whatsapp</Link></Card>
-                            <Card className="w-[300px] text-center p-2"><Link target="_blank" href={"https://website.com"}>Site</Link></Card>
-                            <Card className="w-[300px] text-center p-2"><Link target="_blank" href={"https://onlyfans.com/u329291482"}>Onlyfans</Link></Card>
+                            {profile.links.map((profileLink, index) => (
+                                <Card className="w-[300px] text-center p-2"><Link target="_blank" href={profileLink.link}>{profileLink.name}</Link></Card>
+                            ))}
                         </CardContent>
                     </div>
                 </Card>
---
+++ nextlinktree-front/src/app/[username]/services/PublicProfileService.ts
@@ -0,0 +1,39 @@
+
+
+export interface PublicProfile {
+    username: string;
+    imageURL: string;
+    description: string;
+    links: PublicProfileLinks[];
+}
+
+export interface PublicProfileLinks {
+    name: string;
+    link: string;
+}
+
+export function getPublicProfile(username: string): PublicProfile {
+    return {
+        username: username,
+        description: "Maior cachorro do brasil",
+        imageURL: "https://github.com/shadcn.png",
+        links: [
+            {
+                name: "Instagram",
+                link: "https://instagram.com"
+            },
+            {
+                name: "Facebook",
+                link: "https://facebook.com"
+            },
+            {
+                name: "Whatsapp",
+                link: "https://web.whatsapp.com/"
+            },
+            {
+                name: "Onlyfans",
+                link: "https://onlyfans.com"
+            }
+        ]
+    }
+}
